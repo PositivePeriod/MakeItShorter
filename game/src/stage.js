0a7432bedbcd7aefbcdf4e3e373091e40e59766e
PositivePeriod <positiveperiod.dev@gmail.com>
2021-09-30 22:46:29 +0900

    feat: use webworker, rename data
    v1.1

--- game/src/stage.js
+++ game/src/stage.js
@@ -1,17 +1,19 @@
 export class Stage {
     constructor() {
-        this.stageID = 1;
-        this.maxStageID = 3;
+        this.stageList = ['square', 'factorial', 'prime', 'random']
+        this.minStageID = 1;
+        this.maxStageID = this.stageList.length;
+        this.stageID = this.minStageID;
     }
 
     async content() {
-    var filePath = `./data/${this.stageID}.json`;
-    var response = await fetch(filePath)
-    return response.json()
+        var filePath = `./data/${this.stageList[this.stageID - 1]}.json`;
+        var response = await fetch(filePath)
+        return response.json()
     }
 
     previous() {
-        this.stageID = Math.max(--this.stageID, 1);
+        this.stageID = Math.max(--this.stageID, this.minStageID);
     }
 
     next() {
